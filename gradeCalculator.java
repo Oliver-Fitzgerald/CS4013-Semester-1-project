5c61788eaa258431802b617ef13485d556c1e9a1
Oliver-fits7 <150403145+Oliver-fits7@users.noreply.github.com>
2023-11-27 09:58:13 +0000

    Updated gradeCaluclaor
    Realised I was the problem.

--- gradeCalculator.java
+++ gradeCalculator.java
@@ -1,74 +1,73 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.function.DoubleUnaryOperator;
 
-public class gradeCalculator{
+public class gradeCalculator {
     //calculates students grades and QCA
 
-    //I will need to get all the modules a student takes in one semseter
-    //To get the the modules taken in the same semester
-    //where year and semstser are equal
     /**
-      *calculates the qca of a semster
-      * @param studentId
-      * @return QCA
-      */
+     * calculates the qca of a semester
+     * @param modules
+     * @param semsterYear
+     * @param  semsterSemester
+     * @return a double representing the semesters QCA
+     */
+    public double semesterQca(Arraylist<StudentModules> modules, int semsterYear, int semsterSemester) {
+        double semesterQca = 0 ;
+        for (StudentModule module : modules) {
+            semesterQca += moduleGrade(module,semsterYear,semsterSemester) ;
+        }
+
+        return semesterQca / modules.size() ;
+    }
+
+    /**
+     *Gets the qca value for a module
+     * @param module the module that we are get the qca
+     * @param year  the year that the module took place in
+     * @param semester the semester that the module took place in
+     * @return the qca of the module
+     */
+    public static double moduleGrade (StudentModule module,int year, int semester){
+
+        double[] testWeightings = module.getTestWeightings();
+        double[] grades = module.getGrades();
+
+        String moduleGradingScheme = module.getGradingScheme();
+        //Puts the grading scheme into a hash map and converts grade e.g(a1)
+        //and converts to a qca value e.g(4.0)
+        //      QPV      %GRADE
+        HashMap<Double, Double> gradingScheme = new HashMap<Double, Double>();
+        for (int number = 0; number <= 11; number++) {
+            gradingScheme.put(Double.parseDouble(moduleGradingScheme.substring(number, number + 2)), Double.parseDouble(moduleGradingScheme.substring(number + 3, number + 5)));
+        }
+
+        //gets the total grade for a module from tests completed using
+        // the test weightings as a guide
+        double totalGrade = 0;
+        for (int number = 0; number <= grades.length; number++) {
+            totalGrade += grades[number] * (testWeightings[number] / 100);
+
+        }
+
+        //gets the appropriate qpv based on the modules grading scheme and the students results
+        for (Map.Entry<Double,Double> entry:gradingScheme.entrySet()) {
+
+            //80 <= 92    is true
+            if (entry.getValue() <= totalGrade)
+                //return 4.0
+                return entry.getKey() ;
+            }
+
+            return 0;
+        }
+    }
+/*
+                Notes
+
     // (result 1 (testWeighting / 100) + results 2  (testWeighting / 100) + ...) / number of results
     //= module grade
     //module grade -> grade e.g(a1,a2,a3) //just for student to see
     //module grade -> QPV e.g(3.2,3.6) //used to calculate qca
     //QCA -> (module1 QPV +  module2 QPV) / number of modules
-    public double semesterQca(Arraylist<StudentModules> modules,int semsterYear,int semsterSemester) {
-
-        double[] modulesQpvs = new double[modules.size()] ;
-        int number = 0;
-
-        for (StudentModule module:modules){
-
-            if (module.getYear() == semsterYear && module.getSemester() == semsterSemester) {
-                double[] testWeightings = module.getTestWeightings();
-                double[] grades = module.getGrades();
-                double moduleGrade = 0 ;
-                double moduleQpv = 0 ;
-                String moduleGradingScheme = module.getGradingScheme() ;
-
-                /*
-                 * if <= then the student hasn't completed every test yet and this programme will
-                 * allow a student to see their qca so far it also may be the case that a student
-                 * didn't complete a test and the teacher didn't submit a grade for them at all apose
-                 * to giving them a 0 ;
-                 */
-                if (grades.length > testWeightings.length){
-                    System.out.println("Error their are either to many tests or missing grade weightings");
-                    return 0.0 ;
-                }
-                //gets the total grade for a module from tests completed using
-                //the test weightings as a guide
-                for (int i = 0; i <= grades.length)
-                    moduleGrade += grades[i] * (testWeightings[i] / 100) ;
-
-                //gets the appropriate qpv based on the modulues grading scheme
-                HashMap<String,Double> gradingScheme = new HashMap<String,Double>() ;
-                String[] key = new String[11] ;
-                //there are " " qpv value
-                for (int i = 0;i <= (11 * 6);i += 6) {
-                    gradingScheme.put(Double.parseDouble(moduleGradingScheme.substring(i,i + 2)) ,Double.parseDouble(moduleGradingScheme.subString(i + 3,i + 5)))  ;
-                    key[i / 6] = moduleGradingScheme.substring(i,i + 2) ;
-                }
-
-                for (int i = 0;i <= 11 ;i++) {
-                    if (gradingScheme.get(key[i]) <= moduleGrade)
-                        moduleQpv = Double.parseDouble(key[i])  ;
-                }
-
-                moduleQpvs[number] = moduleQpv ;
-                number++ ;
-                //Catch errors and stuff
-            }
-
-
-        }
-
-
-    }
-
-}
+*/
